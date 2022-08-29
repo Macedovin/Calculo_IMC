7f58d84139cb0ffd329bfe697ac2711bd5f03b7c
Vinicius Macedo (VMP Ilustra) <93549002+VMPILUSTRA@users.noreply.github.com>
2022-08-29 19:10:58 -0300

    Acrescentando a lógica da modal de alerta e suas funcionalidades
    Averigando a condicional dos valores de IMC e seus significados, bem como as aoresentações da modal de alerta.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 // IMPORTANDO OS DADOS NECESSÁRIOS
 
-import { Modal } from './modal.js'
+/* ACRESCENTANDO A IMPORTAÇÃO DA MODAL DE ALERTA */
+
+import { Modal, ModalInform } from './modal.js'
 
 import { AlertError } from './alert-erro.js'
 
@@ -13,6 +15,14 @@ const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 
 const inputHeight = document.querySelector('#height')
+
+/* ELEMENTOS DA MODAL INFORM */
+
+const modalInform = document.querySelector('.modal.inform')
+
+const modalInformTitle = modalInform.querySelector('h3')
+
+const modalInformText = modalInform.querySelector('.text')
 
 /* ELEMENTOS REFERNTES A MODAL -> NOVO ARQUIVO MODAL.JS */
 
@@ -63,6 +73,55 @@ form.onsubmit = function (event) {
   /* CHAMANDO A FUNÇÃO DE CÁLCULO DO IMC, UTILIZANDO AS VARIÁVEIS ANTERIORES COMO ARGUMENTO E APRESENTANDO RESULTADO */
 
   const result = calculateIMC(weight, height)
+  
+  /* VARIÁVEIS DE PESO */
+
+  const skinny = result < 18.5
+
+  const helthy = result >= 18.5 && result <= 24.9
+
+  const highWeight = result >= 25 && result <= 29.9
+
+  const obesityOne = result >= 30 && result <= 34.9
+
+  const obesityTwo = result >= 35 && result <= 39.9
+
+  const faty = result >= 40
+
+  /* CONDICIONAIS DE ALERTA */
+  
+  if (helthy) {
+    ModalInform.congrats()
+
+    ModalInform.title.innerText = 'Parabéns!!!'
+
+    ModalInform.text.innerText = 'Peso normal'
+
+    console.log(result, 'Parabéns!!! Peso normal')
+  } else if (result < 18.5 || (result >= 25 && result <= 39.9)) {
+    if (skinny) {
+      ModalInform.text.innerText = 'Abaixo do peso'
+      console.log(result, 'Cuidado! Abaixo do peso')
+    } else if (highWeight) {
+      ModalInform.text.innerText = 'Acima do peso (sobrepeso)'
+      console.log(result, 'Cuidado! Acima do peso (sobrepeso)')
+    } else if (obesityOne) {
+      ModalInform.text.innerText = 'Obesidade I'
+      console.log(result, 'Cuidado! Obesidade I')
+    } else if (obesityTwo) {
+      ModalInform.text.innerText = 'Obesidade II'
+      console.log(result, 'Cuidado! Obesidade I')
+    }
+
+    alertWorry()
+  } else if (faty) {
+    ModalInform.danger()
+
+    ModalInform.title.innerText = 'Perigo!'
+
+    ModalInform.text.innerText = 'Obesidade III'
+    console.log(result, 'Perigo! Obesidade III')
+  }
 
   displayResultMessage(result)
 }
@@ -70,6 +129,14 @@ form.onsubmit = function (event) {
 /* LINHAS DE CÓDIGO PARA FUNÇÕES UTILITÁRIAS: é UM NÚMERO E CÁLCULO IMC -> NOVO ARQUIVO UTILS.JS */
 
 /* LINHA DE CÓDIGO DE FECHAMENTO DA MODAL -> NOVO ARQUIVO MODAL.JS    */
+
+/* FUNÇÃO PARA O ALERTA DE CUIDADO */
+
+function alertWorry(result) {
+  ModalInform.worry()
+
+  ModalInform.title.innerText = 'Cuidado!'
+}
 
 /* FUNÇÃO PARA MOSTRAR MENSAGEM DE RESULTADO */
 
